72ab7c62b432d83797383dba0a351b81e41490e8
baristaahmadmuhtar <baristaahmadmuhtar@gmail.com>
2025-12-24 05:40:18 +0800

    Update pb.ts

--- services/pb.ts
+++ services/pb.ts
@@ -1,14 +1,21 @@
 import PocketBase from 'pocketbase';
 
 // KONFIGURASI DINAMIS UNTUK VERCEL & LOCALHOST
-// Prioritas:
-// 1. VITE_POCKETBASE_URL (Settingan di Vercel)
-// 2. Default Localhost (Untuk development di laptop)
-const PB_URL = process.env.VITE_POCKETBASE_URL || 'http://127.0.0.1:8090';
+// Perubahan: Menggunakan import.meta.env (Standar Vite) agar .env terbaca
+const PB_URL = import.meta.env.VITE_POCKETBASE_URL || 'http://127.0.0.1:8090';
 
 console.log(`[DATABASE] Connecting to: ${PB_URL}`);
 
 export const pb = new PocketBase(PB_URL);
+
+// --- TAMBAHAN PENTING (Agar tidak diblokir Ngrok) ---
+pb.beforeSend = function (url, options) {
+    options.headers = Object.assign({}, options.headers, {
+        'ngrok-skip-browser-warning': 'true',
+    });
+    return { url, options };
+};
+// ----------------------------------------------------
 
 // Matikan auto-cancellation agar request yang cepat tidak saling membatalkan
 pb.autoCancellation(false);
